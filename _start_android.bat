8add9ff49e8ddae60f0c6bcfa3c65e50a16f4f64
BronOfFireUnkn <bronoffire_unkn@petalmail.com>
2023-10-04 15:55:44 +0800

    dev2

--- _start_android.bat
+++ _start_android.bat
@@ -1,2 +1,14 @@
+@echo off
+
 git pull https://github.com/BronOfFireUnkn/SeeleAssistantCode.git
+
+:: Deactivate the virtual environment
+call .\Dev-seele\Scripts\deactivate.bat
+
+:: Calling external python program to check for local modules
+python .\setup\check_local_modules.py --no_question
+
+:: Activate the virtual environment
+call .\Dev-seele\Scripts\activate.bat
+
 py main.py
---
+++ 安装依赖文件.bat
@@ -0,0 +1,3 @@
+python -m venv Dev-seele
+Dev-seele\Scripts\Activate
+pip install -r requirements.txt  -i https://mirrors.aliyun.com/pypi/simple/
